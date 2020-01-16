63f79bb1a9cf7a775bdd4ac8698b4cbd69fcb3dc
Alex Cuenca M <maxpowerxd8y9@gmail.com>
2020-01-16 12:52:21 -0500

    Modificando a una coleccion mas generica con enumerables

--- Ubisoft/Models/ComprasProducto.cs
+++ Ubisoft/Models/ComprasProducto.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 namespace Ubisoft.Models
 {
     public class ComprasProducto
-    {       
-        public Persona Persona { get; set; }
+    {
+        public int Id { get; set; }
+        public Compra Compra { get; set; }
+        public Producto Producto { get; set; }
         public int Cantidad { get; set; }
         public double PrecioProducto { get; set; }
-        //Puede ser opcional :D
-        public List<Producto> Productos { get; set; }
 
     }
 }
