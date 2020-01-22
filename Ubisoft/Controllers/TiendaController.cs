ca522863362a92a68046d853269e7b3568d5823d
Alex Cuenca M <maxpowerxd8y9@gmail.com>
2020-01-22 15:05:39 -0500

    Reparando el ORM

--- Ubisoft/Controllers/TiendaController.cs
+++ Ubisoft/Controllers/TiendaController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Ubisoft.Models;
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,22 +11,18 @@ namespace Ubisoft.Controllers
 {
     public class TiendaController : BaseController
     {
-        public TiendaController(UbisoftContext context): base(context){}        
-        // GET: /<controller>/
-        public IActionResult Index()
+        public TiendaController(UbisoftContext context) : base(context) { }
+        public IActionResult Index()//Linkear una consulta de muchos a muchos 
         {
             var productos = Context.ProductoConCaracteristicas;
-            return View(productos);
-        }
-        private List<Producto> cargarProducto()
-        {
-            
-            return new List<Producto>
-            {
-                new Producto { Id = 1, Titulo = "Calculadora Gráfica HP Prime G2 2019",Precio= 215, Ruta = "/img/calculadora-grafica-hp-prime-solohpcom-01_2.jpg" },
-                new Producto { Id = 2, Titulo = "Pendrive HP 16GB Modelo X750W", Ruta = "/img/pendrive-hp-x750w-16gb-solohpcom-01.jpg", Precio = 12 },
-                new Producto { Id = 3, Titulo = "Pendrive HP 32GB Modelo X750W", Ruta = "/img/pendrive-hp-x750w-16gb-solohpcom-01.jpg", Precio = 22 },
-            };
+            //Se tiene que modificar el contexto para cargar datos de manera diligente
+            Context.Caractersisticas.Include(b => b.ProductoConCaracteristicas).ToList();
+            Context.Productos.Include(b => b.ProductoConCaracteristicas).ToList().GetRange(2, 3);
+
+            var query = from feature in productos
+                        group feature by feature.Producto into detalles
+                        select detalles;
+            return View(query);
         }
     }
 }
--- Ubisoft/Models/Producto.cs
+++ Ubisoft/Models/Producto.cs
@@ -14,7 +14,7 @@ namespace Ubisoft.Models
         public string Ruta { get; set; }
         public double Precio { get; set; }
         public virtual List<ComprasProducto> ComprasProductos { get; set; }
-
+        public virtual List<ProductoConCaracteristica> ProductoConCaracteristicas { get; set; }
         //Puede ser opcional :D 
         //En ese caso, modificar la vista parcial ProductoTienda.cshtml        
         //public Producto() => Features = new List<string>();
